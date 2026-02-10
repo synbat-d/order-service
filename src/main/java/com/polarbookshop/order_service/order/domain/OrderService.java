7b864cb8b112e4cf8e4d3aa36256625255fbe638
Symbat Dulatuly <synbat1999@gmail.com>
2026-02-10 21:19:00 +0500

    chapter 8 finished

--- src/main/java/com/polarbookshop/order_service/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/order_service/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.order_service.order.domain;
 
+import com.polarbookshop.order_service.book.Book;
+import com.polarbookshop.order_service.book.BookClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -9,16 +11,26 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final BookClient bookClient;
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        return Mono.just(buildRejectedOrder(isbn, quantity)).flatMap(orderRepository::save);
+        return bookClient.getBookByIsbn(isbn)
+                .map(book -> buildAcceptedOrder(book, quantity))
+                .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
+                .flatMap(orderRepository::save);
     }
 
     public static Order buildRejectedOrder(String bookIsbn, int quantity) {
         return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
+    }
+
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.of(book.isbn(),
+                book.title() + " - " + book.author(),
+                book.price(), quantity, OrderStatus.ACCEPTED);
     }
 }
