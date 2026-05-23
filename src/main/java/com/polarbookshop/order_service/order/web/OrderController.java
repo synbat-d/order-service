0c86e89c3ba9ce9bcf373e63b5d2cb2ccd932102
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-23 20:25:45 +0500

    Chapter 13 exposing actuator enppoints without auth

--- src/main/java/com/polarbookshop/order_service/order/web/OrderController.java
+++ src/main/java/com/polarbookshop/order_service/order/web/OrderController.java
@@ -4,6 +4,8 @@ import com.polarbookshop.order_service.order.domain.Order;
 import com.polarbookshop.order_service.order.domain.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +16,14 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     private final OrderService orderService;
 
     @GetMapping
     public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        log.info("Fetching all orders for user {}", jwt.getSubject());
         return orderService.getAllOrders(jwt.getSubject());
     }
 
@@ -25,6 +31,7 @@ public class OrderController {
     public Mono<Order> submitOrder(
             @RequestBody @Valid OrderRequest orderRequest
     ) {
+        log.info("Submitting order for ISBN {} with quantity {}", orderRequest.isbn(), orderRequest.quantity());
         return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
     }
 }
